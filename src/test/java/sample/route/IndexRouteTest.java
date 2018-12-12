a7b8c117548d1ce8eaa24cc6f78a95094b7c1f9e
Rishikesh Dhokare <rishikesh.m.dhokare@gmail.com>
2018-12-12 10:25:46 +0530

    assert the response

--- src/test/java/sample/route/IndexRouteTest.java
+++ src/test/java/sample/route/IndexRouteTest.java
@@ -1,35 +1,36 @@
 package sample.route;
 
-import io.dropwizard.testing.junit.ResourceTestRule;
+import io.dropwizard.testing.junit.DropwizardAppRule;
+import org.glassfish.jersey.client.JerseyClient;
+import org.glassfish.jersey.client.JerseyClientBuilder;
+import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Test;
+import sample.Main;
+import sample.config.AppConfiguration;
 
-import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Response;
-import java.util.Map;
 
-import static org.junit.Assert.*;
+import static io.dropwizard.testing.ResourceHelpers.resourceFilePath;
+import static org.junit.Assert.assertEquals;
 
 public class IndexRouteTest {
 
+    private JerseyClient client;
+
     @ClassRule
-    public static final ResourceTestRule resources = ResourceTestRule.builder()
-            .addResource(new IndexRoute())
-            .build();
+    public static final DropwizardAppRule<AppConfiguration> RULE = new DropwizardAppRule<>(Main.class, resourceFilePath("app-config-for-test.yml"));
+
+    @Before
+    public void setUp() {
+        client = new JerseyClientBuilder().build();
+    }
 
     @Test
     public void shouldPingTheApp() {
-        // when
-        Response response = getTarget("ping").request().get(Response.class);
-
-        // then
+        String uri = String.format("http://localhost:%d/ping", RULE.getLocalPort());
+        Response response = client.target(uri).request().get();
         assertEquals(200, response.getStatus());
-        Map<String, String> responseBody = response.readEntity(Map.class);
-        assertEquals("Inventory integration between LiveDooh and AIDA", responseBody.get("description"));
-    }
-
-    private WebTarget getTarget(String path) {
-        String uri = String.format("http://localhost:%d/%s", 7013, path);
-        return resources.client().target(uri);
+        assertEquals("pong", response.readEntity(String.class));
     }
 }
